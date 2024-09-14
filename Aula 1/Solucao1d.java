efa37afd03316ba830f31a8a9ed8ef9609cc4b0e
Douglas Mota <114318340+SoyDougMota@users.noreply.github.com>
2024-09-14 13:11:36 -0300

    Solucao1d.java

--- Aula 1/Solucao1d.java
+++ Aula 1/Solucao1d.java
@@ -3,8 +3,8 @@ import javax.swing.JOptionPane;
 public class Solucao1d {
     public static void main(String[] args) {
         //Entrada Peso
-        String input = JOptionPane.showInputDialog("Digite o seu peso (em kg):");
-        double peso = Double.parseDouble(input);
+        String PesoReal = JOptionPane.showInputDialog("Digite o seu peso (em kg):");
+        double peso = Double.parseDouble(PesoReal);
         //Saida na Tela
         JOptionPane.showMessageDialog(null, "O peso informado foi " + peso + " kg.");
     }
