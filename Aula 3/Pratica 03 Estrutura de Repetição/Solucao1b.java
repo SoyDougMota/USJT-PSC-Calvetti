cb5d96724eaaa21d0a1da9a95a250bed295fd453
Douglas Mota <114318340+SoyDougMota@users.noreply.github.com>
2024-10-03 14:16:13 -0300

    Delete Aula 3/Pratica 03 Estrutura de Repetição directory

--- Aula 3/Pratica 03 Estrutura de Repetição/Solucao1b.java
+++
@@ -1,9 +0,0 @@
-public class Solucao1b {
-    public static void main(String[] args) {
-        int count = 0;
-        for (int i = 2; count < 100; i += 2) {
-            System.out.println(i);
-            count++;
-        }
-    }
-}
--- Aula 3/Pratica 03 Estrutura de Repetição/Solucao1g.java
+++
@@ -1,7 +0,0 @@
-public class Solucao1g {
-    public static void main(String[] args) {
-        for (int i = 5; i <= 500; i += 5) {
-            System.out.println(i);
-        }
-    }
-}
